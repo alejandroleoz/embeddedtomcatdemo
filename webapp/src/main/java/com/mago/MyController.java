2100e3f1d0bf725b1847693708d6afc795a16885
Alejandro Leoz <alejandroleoz@gmail.com>
2019-10-25 17:46:24 -0300

    Fixing issue when serializing as JSON

--- webapp/src/main/java/com/mago/MyController.java
+++ webapp/src/main/java/com/mago/MyController.java
@@ -2,6 +2,9 @@ package com.mago;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/controller")
 public class MyController {
@@ -16,8 +19,15 @@ public class MyController {
 
     @GET
     @Path("person")
-//    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Person getPerson() {
-        return new Person("Alenajdro", "Leoz");
+        return new Person("Alejandro", "Ezequiel");
+    }
+
+    @GET
+    @Path("person/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person getPerson(@PathParam("name") String name) {
+        return new Person(name, "Ezequiel");
     }
 }
